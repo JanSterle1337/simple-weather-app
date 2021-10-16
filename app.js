20bcbcba35b64a801390c5871f1ccba626d68c19
Jan Sterle <86469319+JanSterle1337@users.noreply.github.com>
2021-10-16 22:41:33 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+let weather = {
+    "apiKey": "0f37a6eb6caccc6570dea16c0fbd455d",
+    
+    fetchWeather: function() {
+
+
+        let city = document.getElementById("getCity").value;
+
+        if (city) {
+            fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid=" + this.apiKey
+        )
+
+        .then((response)  => {
+            if (!response.ok) {
+                alert("No weather found.");
+                throw new Error("No weather found");
+            }
+
+            return response.json();
+        })
+
+        .then((data) => this.displayWeather(data));
+
+        } else {
+            alert ("No weather found for this location.");
+        }
+
+        
+    },
+
+
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+
+        
+        document.querySelector(".city").innerText = name;
+
+        document.querySelector(".weather-description").innerText = description;
+       
+        document.querySelector(".city-temperature").innerText = temp + "°C";
+
+        document.querySelector(".city-humidity").innerText = "humidity: " + humidity + "%";
+
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+
+    }
+};
